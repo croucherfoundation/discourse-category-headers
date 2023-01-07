e1b66f7ed9af9946e3fa5e6bb5c80d3163b908f4
Thiha <thiha.dve@gmail.com>
2023-01-07 16:11:15 +0630

    first commit

---
+++ javascripts/discourse/initializers/categories-header.js
@@ -0,0 +1,32 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+import { default as Category } from "discourse/models/category";
+
+export default {
+  name: "categories-header",
+  initialize() {
+    withPluginApi("0.8.7", (api) => {
+
+      const idList = settings.featured_categories.split("|");
+      const featuredCategories = idList.map(id => Category.findById(id));
+
+      api.registerConnectorClass("below-site-header", "categories-header", {
+        setupComponent(args, component) {
+          api.onPageChange((url, title) => {
+            if (url === "/" || url === "/latest" || url === "/new" || url === "/unread" || url === "/top") {
+              $("html").addClass("categories-header");
+              // add a class to the HTML tag for easy CSS targetting
+              component.set("categories", featuredCategories);
+              console.log(featuredCategories);
+            } else {
+              // If the page doesn't match the urls above
+              $("html").removeClass("categories-header");
+              // Remove our custom class
+              component.set("categories", []);
+              // set the categories to an empty array to disable rendering
+            }
+          });
+        },
+      });
+    });
+  },
+};
